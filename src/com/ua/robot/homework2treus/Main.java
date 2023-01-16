1672a8dd2e200cdb48d1e368579d07fbbd874504
Alla Treus <stilnykua@gmail.com>
2023-01-16 11:39:06 +0200

    Initial commit

---
+++ src/com/ua/robot/homework2treus/Main.java
@@ -0,0 +1,8 @@
+package com.ua.robot.homework2treus;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
+    }
+}

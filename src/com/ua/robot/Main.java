9a5fd2e2433ef82705eb0ad2d4dbea9b93961617
Alla Treus <stilnykua@gmail.com>
2023-01-18 19:18:52 +0200

    Initial commit

--- src/com/ua/robot/Main.java
+++ src/com/ua/robot/Main.java
@@ -3,7 +3,7 @@ package com.ua.robot;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
     }
 
 }
